6bec88008397a54b28bd1e7951275c7f8b0e15f8
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-02 15:54:28 +0700

    make the first node point to the new node

--- 218_doublelinkedlist.cpp
+++ 218_doublelinkedlist.cpp
@@ -17,5 +17,22 @@ void addNode() {
     cout << "\nEnter the roll number of the student: "; 
     cin >> newNode->noMhs; // assign value to the data field of the new node
     cout << "\nEnter the name of the student: ";
-    cin >> newNode->name;
+    cin >> newNode->name; // assign value to the data field of the new node
+
+
+    // insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs) {
+
+        if (START !=NULL && newNode->noMhs == START->noMhs) {
+            cout << "\033[31mDuplicate roll number not numbers not allowed\033[0m" << endl;
+            return;
+        }
+        // if the list is empty, make the new node the START
+        newNode->next = START; // step 3 : make the new node point to the first node
+        if (START != NULL) {
+            START->prev = newNode; // step 4: make the first node point to the new node
+        }
+        
+    }
 }
+
