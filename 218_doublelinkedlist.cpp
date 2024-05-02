805d9a714bb60396d835623563166ab605319fcf
nayisntaround87 <naylasalwahayati@gmail.com>
2024-05-02 15:36:03 +0700

    addnode

---
+++ 218_doublelinkedlist.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node
+{
+    int noMhs;
+    string name;
+    Node* next;
+    Node* prev;
+};
+
+Node* START = NULL;
+
+void addNode() {
+    Node* newNode = new Node(); // step 1 create a new node
+    cout << "\nEnter the roll number of the student: "; 
+    cin >> newNode->noMhs; // assign value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;
+}
